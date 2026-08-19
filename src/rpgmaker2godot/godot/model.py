eb31d4b9ef8897ebf9048dc68719be879ae0e045
ultra-sonic-28 <4x4.chris@gmail.com>
2026-08-19 15:36:03 +0200

    Fix duplicated class definition in godot model

--- src/rpgmaker2godot/godot/model.py
+++ src/rpgmaker2godot/godot/model.py
@@ -26,33 +26,6 @@ class GodotAtlasMapping:
     tiles: tuple[GodotAtlasTile, ...]
 
 
-from dataclasses import dataclass
-
-from ..model import TileRef
-
-
-@dataclass(frozen=True)
-class GodotAtlasTile:
-    ref: TileRef
-
-    atlas_x: int
-    atlas_y: int
-
-    width: int
-    height: int
-
-
-@dataclass(frozen=True)
-class GodotAtlasMapping:
-    tile_width: int
-    tile_height: int
-
-    width: int
-    height: int
-
-    tiles: tuple[GodotAtlasTile, ...]
-
-
 @dataclass(frozen=True)
 class GodotAtlasSource:
     texture_path: Path
